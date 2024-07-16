a0c78c63de8fe03793f24626a10c75fd7c97a22b
dillontherrien <dillontherrien5@gmail.com>
2024-07-15 21:44:23 -0400

    remove emoji test

--- src/api/api.test.ts
+++ src/api/api.test.ts
@@ -13,13 +13,3 @@ describe('GET /api/v1', () => {
       }, done);
   });
 });
-
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/api/v1/emojis')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
-  });
-});
